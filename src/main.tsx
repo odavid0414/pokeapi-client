52a9a840e2172e801b2d3bead7d681cafdeeecc6
David Olah <59338719+odavid0414@users.noreply.github.com>
2025-09-20 13:29:05 +0200

    install packages, set mantine and app providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx'
-
+import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './store/store.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <MantineProvider defaultColorScheme="dark">
+    <ReduxProvider store={store}>
+      <ModalsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalsProvider>
+    </ReduxProvider>
+  </MantineProvider>
 
 )
